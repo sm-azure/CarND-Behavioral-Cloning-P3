f6e159c5557a6000638dfda08bb7f017f66ddbbb
Ubuntu <ubuntu@ip-172-31-41-100.eu-central-1.compute.internal>
2018-07-10 15:13:59 +0000

    model json, weights and video

--- drive.py
+++ drive.py
@@ -63,8 +63,8 @@ def telemetry(sid, data):
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
         # Convert image into grayscale
-        image_array = np.dot(image_array[...,:3], [0.299, 0.587, 0.114])
-        image_array = image_array.reshape(160, 320, 1)
+        #image_array = np.dot(image_array[...,:3], [0.299, 0.587, 0.114])
+        #image_array = image_array.reshape(160, 320, 1)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
